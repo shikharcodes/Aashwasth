8e21708998394490fd5a68ad0851d7c99ea3750e
Shikhar Saxena <shikhar06jan@gmail.com>
2022-02-20 00:49:04 +0530

    Updated Chat.svelte

--- src/Chat.svelte
+++ src/Chat.svelte
@@ -1,8 +1,8 @@
 <script>
   import Login from './Login.svelte';
   import ChatMessage from './ChatMessage.svelte';
-  import { onMount } from 'svelte';
-  import { username, user } from './user';
+  import {onMount} from 'svelte';
+  import {username, user} from './user';
   import debounce from 'lodash.debounce';
 
   import GUN from 'gun';
@@ -17,7 +17,7 @@
   let unreadMessages = false;
 
   function autoScroll() {
-    setTimeout(() => scrollBottom?.scrollIntoView({ behavior: 'auto' }), 50);
+    setTimeout(() => scrollBottom?.scrollIntoView({behavior: 'auto'}), 50);
     unreadMessages = false;
   }
 
@@ -30,34 +30,34 @@
 
   onMount(() => {
     var match = {
-      // lexical queries are kind of like a limited RegEx or Glob.
       '.': {
         // property selector
-        '>': new Date(+new Date() - 1 * 1000 * 60 * 60 * 3).toISOString(), // find any indexed property larger ~3 hours ago
+        '>': new Date(+new Date() - 1 * 1000 * 60 * 60 * 3).toISOString(),  // find any indexed property larger ~3 hours ago
       },
       '-': 1, // filter in reverse
     };
 
-    // Get Messages
+    // get messages
     db.get('chat')
       .map(match)
       .once(async (data, id) => {
         if (data) {
-          // Key for end-to-end encryption
+          // key for end-to-end encryption
           const key = '#foo';
 
           var message = {
             // transform the data
-            who: await db.user(data).get('alias'), // a user might lie who they are! So let the user system detect whose data it is.
-            what: (await SEA.decrypt(data.what, key)) + '', // force decrypt as text.
-            when: GUN.state.is(data, 'what'), // get the internal timestamp for the what property.
+            who: await db.user(data).get('alias'), // a user might lie who they are! So the user system detects whose data it is.
+            what: (await SEA.decrypt(data.what, key)) + '', // force decrypt as text
+            when: GUN.state.is(data, 'what'), // get the internal timestamp for the what property
           };
 
           if (message.what) {
             messages = [...messages.slice(-100), message].sort((a, b) => a.when - b.when);
             if (canAutoScroll) {
               autoScroll();
-            } else {
+            } 
+            else {
               unreadMessages = true;
             }
           }
@@ -87,9 +87,9 @@
     </main>
 
     <form on:submit|preventDefault={sendMessage}>
-      <input type="text" placeholder="Type a message..." bind:value={newMessage} maxlength="100" />
+      <input type="text" placeholder="Share your thoughts..." bind:value={newMessage} maxlength="100" />
 
-      <button type="submit" disabled={!newMessage}>💥</button>
+      <button type="submit" disabled={!newMessage}>💭</button>
     </form>
 
 
